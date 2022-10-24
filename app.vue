bcc0f6ded1133240df1fe826a492b175e9e6c5cc
Ben Sukstorf <bsukstorf@gmail.com>
2022-10-24 21:57:41 +0200

    Initial commit

---
+++ app.vue
@@ -0,0 +1,53 @@
+<script>
+import photosCatalogue from 'assets/photos-catalogue.json'
+
+export default {
+  data() {
+    return {
+      photosCatalogue,
+    }
+  },
+}
+</script>
+
+<template>
+  <div class="container">
+    <div
+      class="inner-content"
+      v-for="(photo, index) in photosCatalogue.default"
+      :key="index"
+    >
+      <ImageViewer :image="photo.filename" />
+      <p>{{ photo.description }}</p>
+      <p>{{ photo.year }}</p>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+body,
+html {
+  margin: 0;
+  padding: 0;
+}
+
+.container {
+  display: flex;
+  height: 100vh;
+  width: 100vw;
+  background: rgba(24, 23, 23, 1);
+  scroll-snap-type: x mandatory;
+  overflow-y: scroll;
+
+  .inner-content {
+    height: 100vh;
+    width: 100vw;
+    scroll-snap-align: start;
+
+    p {
+      font-family: Georgia, 'Times New Roman', Times, serif;
+      color: white;
+    }
+  }
+}
+</style>
---
+++ components/image-viewer.vue
@@ -0,0 +1,62 @@
+<script>
+export default {
+  name: 'ImageViewer',
+  props: {
+    image: {
+      type: String,
+      default: 'error.jpg',
+    },
+  },
+
+  computed: {
+    backgroundProp() {
+      return {
+        '--this-image-url': `url('photos/${this.image}')`,
+      }
+    },
+  },
+}
+</script>
+
+<template>
+  <div class="image-container">
+    <div class="content background" :style="backgroundProp"> </div>
+    <div class="text-container">
+      <!-- <p>{{ image }}</p> -->
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.image-container {
+  height: 100vh;
+  width: 100vw;
+  margin: 0 auto;
+  position: relative;
+  display: flex;
+}
+
+.content {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+
+  height: 100vh;
+  width: 100vw;
+  max-height: 80vh;
+  max-width: 87vw;
+}
+
+.background {
+  background-image: var(--this-image-url);
+  // background-size: auto 80vh;
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center;
+}
+
+.text-container {
+  position: relative;
+}
+</style>

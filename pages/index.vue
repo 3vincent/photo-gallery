a57e7be59bd72f359ffa5dd8eb6861cc6c69f3dc
Ben Sukstorf <bsukstorf@gmail.com>
2024-03-02 17:07:17 +0100

    Add redirect from top level domain `/` to first gallery

---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts">
+const photoCatalogStore = usePhotoCatalogStore()
+const { galleryNames } = storeToRefs(photoCatalogStore)
+
+await navigateTo(`/${galleryNames.value[0]}`)
+</script>

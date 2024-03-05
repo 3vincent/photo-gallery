9d452b48b53908d890a7f6d3f8c145e09588e537
Ben Sukstorf <bsukstorf@gmail.com>
2024-03-06 00:38:21 +0100

    Move global styles to global CSS file

--- app.vue
+++ app.vue
@@ -5,23 +5,3 @@
     </NuxtLayout>
   </div>
 </template>
-
-<style lang="scss">
-body,
-html {
-  margin: 0;
-  padding: 0;
-  width: 100%;
-  overflow-x: hidden;
-}
-
-div {
-  margin: 0;
-  padding: 0;
-  display: block;
-}
-
-* div {
-  box-sizing: border-box;
-}
-</style>
